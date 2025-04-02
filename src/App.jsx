11efe903b856373f4a4e2bd4687854529668a733
Yeli.James <jbmalvar.cs.washington.edu>
2025-04-02 02:12:19 -0700

    Laptop work on this tmmr

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [mangaList, setMangaList] = useState([]);
+
+  useEffect(() => {
+    const fetchMangaData = async () => {
+      const response = await fetch(
+        "https://api.jikan.moe/v4/manga"
+      );
+      const json = await response.json();
+      setMangaList(json.data);
+    };
+
+    fetchMangaData().catch(console.error);
+  }
+  , []);
+
   return (
     <>
       <div className="App">
@@ -14,15 +29,21 @@ function App() {
             <h1>
               Mangas
               <span className = "buttons">
-                <button className = "button">Prev 50</button>
-                <button className = "button">Next 50</button>
+                <button className = "button">Prev 25</button>
+                <button className = "button">Next 25</button>
               </span>
             </h1>
-            <h2 className = "SearchHeader">Search: 
-              <input></input>
+            <div className = "CollectiveInfo">
+              <div className="infoBox">Highest Rated: {}</div>
+              <div className="infoBox">Average Score: {}</div>
+              <div className="infoBox">Number Completed: {}</div>
+            </div>
+            <h2 className = "searchHeader">Search:
+              <input className = "searchInput"></input>
+              <button className = "searchButton">Search</button>
             </h2>
             <table border="0" cellpadding="0" cellspacing="0" width="100%" class="table">
-              <tbody>
+              <thead>
                 <tr class = "header">
                   <td class = "rankHeader">Rank</td>
                   <td class = "titleHeader">Title</td>
@@ -30,6 +51,17 @@ function App() {
                   <td class = "authorHeader">Author</td>
                   <td class = "statusHeader">Status</td>
                 </tr>
+              </thead>
+              <tbody>
+                {mangaList.map((manga, index) => (
+                  <tr key={manga.mal_id}>
+                    <td>{index + 1}</td>
+                    <td>{manga.title}</td>
+                    <td>{manga.score || 'N/A'}</td>
+                    <td>{manga.authors?.[0]?.name || 'Unknown'}</td>
+                    <td>{manga.status}</td>
+                  </tr>
+                ))}
               </tbody>
             </table>
           </div>
